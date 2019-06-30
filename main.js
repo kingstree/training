4712706bcfd345ccf4ab05364218ff2d324dfcb3
kingstree <ser.kingstree@gmail.com>
2019-06-30 22:24:08 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.greet = function () {
+        console.log("Hi! I'm " + this.name);
+    };
+    return Person;
+}());
+var person = new Person('Sam');
+person.greet();
